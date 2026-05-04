999fe6fe0bf5b81e412be1b9441a65ae66976341
Vadym Moshynskyi <vmoshynskyi@indocresearch.org>
2026-05-04 19:13:35 +0200

    IEBH-476: Revert email related changes

--- api/api_vm/vm.py
+++ api/api_vm/vm.py
@@ -71,10 +71,7 @@ async def reset_password(
         )
     except Exception as e:
         error_msg = f'Error sending email: {e}'
-        logger.error(error_msg)
-        # TODO: revert this back to internal error after email issue is resolved
-        # currently set to allow password reset to proceed even if email fails
-        # return Response(content=error_msg, status_code=EAPIResponseCode.internal_error.value)
+        return Response(content=error_msg, status_code=EAPIResponseCode.internal_error.value)
 
     response = await auth_client.reset_password(data=body)
     return JSONResponse(content=response.json(), status_code=response.status_code)
